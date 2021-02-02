a5c52df6f861b5a17192a76bcfbacb94a0049718
Michael Jones <mtjones1979@yahoo.com>
2021-02-02 14:20:52 -0600

    cleaned up displayweather

--- script.js
+++ script.js
@@ -1,94 +1,69 @@
+var cityListEl = document.querySelector('#cityList');
+var weeklyForecast = document.querySelector('#weeklyForecast');
+var currentWeatherDiv = document.querySelector('.center');
+var cityTemp = document.querySelector('#temp');
+var cityHumidity = document.querySelector('#humidity');
+var cityWind = document.querySelector('#wind');
+var cityUvIndex = document.querySelector('#uvIndex');
 var cityList = [];
 var cityName;
 
 
-function renderCities(){
-    $("#cityList").empty();
-    $("#cityInput").val("");
-
-    for (i = 0; i < cityList.length; i++){
-        var a = $("<a>");
-        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
-        a.attr("data-name", cityList[i]);
-        a.text(cityList[i]);
-        $("#cityList").prepend(a);
-    }
-}
-
-function getCities(){
-    var storedCities = JSON.parse(localStorage.getItem("cities"));
-    if (storedCities !== null) {
-        cityList = storedCities;
-    }
-    renderCities
-}
-
 function displayWeather() {
+    var getLon = response.coord.lon;
+    var getLat = response.coord.lat;
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" +getLat+ "&lon=" +getLon+ "&appid=4140034c75a10c75298c4be081943fce";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=4140034c75a10c75298c4be081943fce";
-
-    var response = await $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        console.log(response);
-
-        var currentWeatherDiv = $("<div class='card-body' id='currentWeather'>");
-        var getCurrentCity = response.name;
-        var date = new Date();
-        var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
-        var getCurrentWeatherIcon = response.weather[0].icon;
-        var displayCurrentWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getCurrentWeatherIcon + "@2x.png />");
-        var currentCityEl = $("<h3 class = 'card-body'>").text(getCurrentCity+" ("+val+")");
-        currentCityEl.append(displayCurrentWeatherIcon);
-        currentWeatherDiv.append(currentCityEl);
-        var getTemp = response.main.temp.toFixed(1);
-        var tempEl = $("<p class='card-text'>").text("Temperature: "+getTemp+"° F");
-        currentWeatherDiv.append(tempEl);
-        var getHumidity = response.main.humidity;
-        var humidityEl = $("<p class='card-text'>").text("Humidity: "+getHumidity+"%");
-        currentWeatherDiv.append(humidityEl);
-        var getWindSpeed = response.wind.speed.toFixed(1);
-        var windSpeedEl = $("<p class='card-text'>").text("Wind Speed: "+getWindSpeed+" mph");
-        currentWeatherDiv.append(windSpeedEl);
-        var getLon = response.coord.lon;
-        var getLat = response.coord.lat;
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    console.log(queryURL);
         
-        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat="+getLat+"&lon="+getLon+"&appid=4140034c75a10c75298c4be081943fce";
-        var uvResponse = await $.ajax({
-            url: uvURL,
-            method: "GET"
-        })
-
+        var cityNameEl = document.querySelector('#cityName');
+        var presentDay = moment().format('MMMM Do YYYY');
+            cityNameEl.textcontent = cityName + presentDay;
+        var getTemp = data.current.temp;
+            cityTemp.textContent = ("Temperature: " +getTemp+ "° F");
+        var getHumidity = data.current.humidity;
+            cityHumidity.textContent = ("Humidity: " +getHumidity+ "%");
+        var getWindSpeed = data.current.wind.speed;
+            cityWind.textContent = ("Wind Speed: " +getWindSpeed+ "MPH");
+        var getUvIndex = data.current.uvi;
+            cityUvIndex.textContent = ("UV Index:  " +getUvIndex);
+        
+        
+       
         // getting UV Index info and setting color class according to value
-        var getUVIndex = uvResponse.value;
-        var uvNumber = $("<span>");
-        if (getUVIndex > 0 && getUVIndex <= 2.99){
-            uvNumber.addClass("low");
-        }else if(getUVIndex >= 3 && getUVIndex <= 5.99){
-            uvNumber.addClass("moderate");
-        }else if(getUVIndex >= 6 && getUVIndex <= 7.99){
-            uvNumber.addClass("high");
-        }else if(getUVIndex >= 8 && getUVIndex <= 10.99){
-            uvNumber.addClass("vhigh");
-        }else{
-            uvNumber.addClass("extreme");
-        } 
-        uvNumber.text(getUVIndex);
-        var uvIndexEl = $("<p class='card-text'>").text("UV Index: ");
-        uvNumber.appendTo(uvIndexEl);
-        currentWeatherDiv.append(uvIndexEl);
-        $("#forecastContainer").html(currentWeatherDiv);
+        // var getUVIndex = uvResponse.value;
+        // var uvNumber = $("<span>");
+        // if (getUVIndex > 0 && getUVIndex <= 2.99){
+        //     uvNumber.addClass("low");
+        // }else if(getUVIndex >= 3 && getUVIndex <= 5.99){
+        //     uvNumber.addClass("moderate");
+        // }else if(getUVIndex >= 6 && getUVIndex <= 7.99){
+        //     uvNumber.addClass("high");
+        // }else if(getUVIndex >= 8 && getUVIndex <= 10.99){
+        //     uvNumber.addClass("vhigh");
+        // }else{
+        //     uvNumber.addClass("extreme");
+        // } 
+        // uvNumber.text(getUVIndex);
+        // var uvIndexEl = $("<p class='card-text'>").text("UV Index: ");
+       
+       
+       
+
+    })
+    
 };
 
-
-// This function is used to pass the city in the history list to the displayWeather function
-
-
+searchCityBtn.addEventListener("click", displayWeather);
 
 // // Trying to search for a city 
 // // shows name of city (appends from search) and the date (need moment format)
 // // Then it shows the towns temp (need calculations for farheit) humidity (response), wind speed, 
 // // uv (separate api for UV also need if for difference in UV - color the box) another "GET"
 // // then it shows 5-day forecast temp, humidity and icon ()
-// // 
